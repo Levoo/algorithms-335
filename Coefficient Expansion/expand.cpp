5ccfffdb39026475e519e1ae13d420efd20a643e
Fernando <fzavala649@gmail.com>
2018-01-31 13:39:55 -0800

    Update expand.cpp

--- Coefficient Expansion/expand.cpp
+++ Coefficient Expansion/expand.cpp
@@ -1,13 +1,20 @@
+/*
+	Authors: Micheal Woodworth
+			 Fernando Cuevas
+	Class:   CPSC 335 - 12884, Algorithms, Tue/Thur
+	Project: Assignment 1, Coefficient expansion  for nth power (n <= 7)
+*/
+
 #include <iostream>
 #include <vector>
-#include <cmath> // take off 
 #define NTH_LIMIT 7
 
 int factorial(int num);
 int expandDong(int n, int k);
 
 int main() {
-	int nth = 0; //set up nth and k for expand dong 
+	int nth = 0; //set up and ask for input
+	int SIZE = 0;
 	std::cout << "Enter the nth value: ";
 	std::cin >> nth;
 	while (nth > NTH_LIMIT) { // error checking 
@@ -15,23 +22,26 @@ int main() {
 		std::cout << "Enter the nth value: ";
 		std::cin >> nth;
 	}
-	int k = 0;
+
 	std::vector<int> coef_list; //vector of leading coefficients 
 	for (int i = 0; i <= nth; i++) {
-		coef_list.push_back(expandDong(nth, i)); // call expand to get the stuff
+		coef_list.push_back(expandDong(nth, i)); // call expand to get coefficient and then add to vector
 	}
-	for (int i = 0; i < (int) coef_list.size(); i++) { // print out results 
-		std::cout << coef_list[i] << " , ";
+	SIZE = (int)coef_list.size(); // store size of vector
+	for (int i = 0; i < SIZE; i++) { // print out results 
+		std::cout << coef_list[i] << "a^" << nth << "*" << "b^" << i;
+		if (i != SIZE - 1) std::cout << " + "; // check to make sure we dont print a + at the very end...
+		nth -= 1;
 	}
-
+	std::cout << std::endl; // add a new line for design 
 	return 0;
 }
 
-int expandDong(int n, int k) { tried to do recursive but ehh :/ 
-	return (factorial(n) / (factorial(k) * factorial(n - k)));
+int expandDong(int n, int k) { // called once ebry time in first for loop
+		return (factorial(n) / (factorial(k) * factorial(n - k))); // return the C(n, k) for nth/k value
 }
 
 int factorial(int num) {  //factorial of n, recursive
-	if (num > 1) return num * factorial(num - 1);
+	if (num > 1) return num * factorial(num - 1); // while > 1 keep multi until we reach 1
 	else return 1;
 }
