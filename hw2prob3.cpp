efda13f85445b856170cd4aa3bec615d5c1ca489
Rukesoundslikeluke <michael.ruke@gmail.com>
2018-02-06 02:34:15 -0800

    Update hw2prob3.cpp

--- hw2prob3.cpp
+++ hw2prob3.cpp
@@ -8,11 +8,11 @@ int a, x, y, max = 0;
 
 void printresults() {	//prints the f(a)
 	cout << "f("<< a << ") = ";
-	for (int i = 1; i < max; ++i) {
+	for (int i = 1; i < max; ++i) {	//beginning "a(a(a(a(a(a......"
 		cout << a << "(";
 	}
 	cout << coef[0] << "*" << a << "+" << coef[1];	//Middle part "ax + b)"
-	for (int i = 2; i < coef.size() - 1; ++i) { 
+	for (int i = 2; i < coef.size() - 1; ++i) { //ending ")+c)+d)+e"
 		cout << ")+" << coef[i];
 	}
 	cout << endl;
@@ -23,7 +23,7 @@ int main() {
 	do {	//Loops to get input for coefficient and power vector
 		cout << "Enter the coefficient and the power of each term(0 0) to stop:";
 		cin >> x >> y;
-		if (y >= max)max = y;
+		if (y >= max)max = y;	//Gets the max power as user inputs
 		coef.push_back(x);
 		power.push_back(y);
 	} while ((x != 0) || (y != 0));
