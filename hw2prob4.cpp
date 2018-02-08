724c5fba99d40a74230385f70b02545c07ae82a7
Fernando <fzavala649@gmail.com>
2018-02-07 16:56:41 -0800

    Done

--- hw2prob4.cpp
+++ hw2prob4.cpp
@@ -1,12 +1,20 @@
+/*
+Authors:
+Fernando Cuevas
+Michael Rodriguez
+Class:   CPSC 335 - 12884, Algorithms, Tue/Thur
+Project: Assignment 2, Prime number problem #4
+*/
+
 #include <iostream>
 #include <vector>
 #include <map>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-using namespace std;
+using namespace std; //ewww
 
-bool isPrime(int n) {
+bool isPrime(int n) { // determine of number is prime 
 	bool prime = true;
 	for (int i = 2; i < n; ++i) {
 		if (n%i == 0) {
@@ -17,36 +25,34 @@ bool isPrime(int n) {
 	return prime;
 }
 
-
-
 int main() {
 	srand(time(NULL));
 	map<int, int> freq;
+	vector<int> freq_count;
 	vector<int> primes;
-	int list[10];
+	int list[15];
 	cout << "List: ";
-	for (int i = 0; i < 10; ++i) {
-		list[i] = rand() % 30 + 10;
-		if (list[i] > 30) list[i] -= 10;
+	for (int i = 0; i < 15; ++i) { // generate the list of numbers 
+		list[i] = rand() % 21 + 10;
+		//if (list[i] > 30) list[i] -= 10;
 		cout << list[i] << " ";
 	}
-	cout << endl << "Primes: "; // got a case where if lets say 23 comes out twice, it will display 23 twice, should we fix?
-	for (int i = 0; i < 10; ++i) {
+	cout << endl << "Primes: "; // get the primes 
+	for (int i = 0; i < 15; ++i) {
 		if (isPrime(list[i])) {
 			cout << list[i] << " ";
 			primes.push_back(list[i]);
 		}
 	}
 	std::cout << std::endl;
-	for(int i = 0; i < 10; i++) { // add to map
-		freq[list[i]] += 1;
+	int max = 0;
+	for (int i = 0; i < (int) primes.size(); i++) { // get prime freq
+		freq[primes[i]] += 1;
+		if (freq[primes[i]] > max) max = freq[primes[i]];
 	}
-	for (auto element : freq) { // range based for loop, basiclly take each element in the map and use it should be O(n) still
-						  // since you can also do auto i = map.begin(); i < size of map; ++
-		if(isPrime(element.first)) // only print frequency if prime a sper part c of question 4
-			cout << "Freq of " << element.first << " is " << element.second << endl; // access the unique key and the value 
-																				 // currently have it display all idk if you wanna display everything above 1
-																				 // or if alll occur once, display all of them 
+	for (auto element : freq) { // print the freq of only the highest prime
+		if (element.second == max)
+			cout << "Freq of " << element.first << ": " << element.second << endl;
 	}
 
 	system("pause");  // why? jk i get it 
