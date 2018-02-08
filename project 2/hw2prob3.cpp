6a1b99ada08cd18e0af59cd0582cccbce09c2e44
Rukesoundslikeluke <michael.ruke@gmail.com>
2018-02-07 20:03:31 -0800

    Update hw2prob3.cpp

--- project 2/hw2prob3.cpp
+++ project 2/hw2prob3.cpp
@@ -4,31 +4,32 @@ using namespace std;
 
 vector<int> coef;
 vector<int> power;
-int a, x, y, max = 0;
+int a, x, y, max = 0, total = 0;
 
-void printresults() {	//prints the f(a)
-	cout << "f("<< a << ") = ";
-	for (int i = 1; i < max; ++i) {	//beginning "a(a(a(a(a(a......"
-		cout << a << "(";
-	}
-	cout << coef[0] << "*" << a << "+" << coef[1];	//Middle part "ax + b)"
-	for (int i = 2; i < coef.size() - 1; ++i) { //ending ")+c)+d)+e"
-		cout << ")+" << coef[i];
-	}
-	cout << endl;
-}
 
 int main() {
 	cout << "To Compute the value of f(a)" << endl;
 	do {	//Loops to get input for coefficient and power vector
 		cout << "Enter the coefficient and the power of each term(0 0) to stop:";
 		cin >> x >> y;
+
 		if (y >= max)max = y;	//Gets the max power as user inputs
 		coef.push_back(x);
 		power.push_back(y);
 	} while ((x != 0) || (y != 0));
 	cout << "Enter the value of a:";
 	cin >> a;
-	printresults();
+	total =coef[0]* a;
+	for (int i = 1;i<=max; ++i) {
+		
+		total = (a * total) + coef[i];
+		cout << ">> RUN: " << i << endl;
+		cout << "Total: " << total << endl;
+		cout << "a: " << a << endl;
+		cout << "coef: " << coef[i] << endl;
+	}
+	cout << "f(a) = " << total;
+
+
 	system("pause");
 }
