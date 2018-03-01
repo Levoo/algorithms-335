cbc1871cb194267aed203d883ac4326aeb0ed5a2
Fernando <fzavala649@gmail.com>
2018-02-28 22:57:16 -0800

    done, printed

--- project4/hw4prob4.cpp
+++ project4/hw4prob4.cpp
@@ -5,76 +5,175 @@ Michael Rodriguez
 Class:   CPSC 335 - 12884, Algorithms, Tue/Thur
 Project: Assignment 4, problem #4
 */
-#include<iostream>
-using namespace std;
 
-struct node{	//node for the list
-	int data = -1;
-	node *next = nullptr;
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+using namespace std; //lel(no)
+
+struct treeNode {
+	std::string name;
+	treeNode* left = nullptr, *right = nullptr;
 };
-class list{	//the list
-public:
-	bool empty = false;
-	node *head;	//pointer to head (top) element in list
-	list(){	//initialize the list empty
-		head = nullptr;
-	}
-	void push(int value){	//uses push to mimic a stack
-		node *temp = new node;
-		node *hold = head;
 
-		temp->data = value;
-		if (head == nullptr){
-			head = temp;
-			temp = nullptr;
+struct queueNode {
+	std::string name;
+	queueNode* next;
+};
+
+class bst {
+public:
+	treeNode* bt_head = nullptr;
+
+	void insert(treeNode* bt, std::string newVal) {
+		if (bt_head == nullptr) {//initializes head of bst
+			bt_head = new treeNode;
+			bt_head->name = newVal;
+			return;
 		}
-		else{
-			temp->next = head;
-			head = temp;
-			temp = nullptr;
+		else { // inserts alphabetically!
+			if (newVal[0] <= bt->name[0]) {
+				if (bt->left == nullptr) { //checks if there is no val in next node
+					treeNode *tmp = new treeNode;//populates and links if none
+					tmp->name = newVal;
+					bt->left = tmp;
+					tmp = nullptr;
+				}
+				else { insert(bt->left, newVal); }//traverse if a node exists
+			}
+			else {
+				if (bt->right == nullptr) { //checks if there is no val in next node
+					treeNode *tmp = new treeNode;//populates and links if none
+					tmp->name = newVal;
+					bt->right = tmp;
+					tmp = nullptr;
+				}
+				else { insert(bt->right, newVal); }//traverse if a node exists
+			}
 		}
 	}
-	void print() {	//prints the list(stack)
-		node *temp = head;
-		empty = true;
-		for (int i = 0; i < 7; ++i) {	//I use 7 to mimic stack size
-			if (temp->data != 0) {
-				empty = false;
-			}
-			cout << temp->data;
-			temp = temp->next;
-			cout << "->";
+	void preorder(treeNode* bt) {
+		if (bt != nullptr) {
+			std::cout << bt->name << " ";//dont display bst as list
+			preorder(bt->left);
+			preorder(bt->right);
 		}
-		cout  <<"NULL" << endl;
 	}
-	void processedcheese() {	//Its a gouda function
-		node *temp = head;
-		for (int i = 0; i < 7; ++i) {	//I use 7 to mimic stack size
-			temp->data -= 3;	// This is the amount processed per pass
-			if (temp->data < 0) {	//makes sure the value doesnt go below 0
-				temp->data = 0;
+	void inorder(treeNode*bt) {//part b
+		if (bt != nullptr) {
+			inorder(bt->left);
+			std::cout << bt->name << " ";
+			inorder(bt->right);
+		}
+	}
+	void dipside(treeNode *bt, int sp) {//part c
+		if (bt != nullptr) {
+			dipside(bt->right, sp += 5);
+			cout << setw(sp) << bt->name << endl;
+			dipside(bt->left, sp);
+		}
+	}
+	int height(treeNode *bt) {//part d & e
+		if (bt == nullptr) { return 0; }
+		else {
+			return  1 + max(height(bt->left), height(bt->right));
+		}
+	}
+	int nodes(treeNode *bt) {//part f
+		if (bt == nullptr) { return 0; }
+		else {
+			return  1 + nodes(bt->left) + nodes(bt->right);
+		}
+	}
+	void descendants(treeNode *bt, string newVal) {//part g
+		if (bt != nullptr) {
+			if (bt->name == newVal) {
+				if (bt->left != nullptr) { preorder(bt->left); }
+				if (bt->right != nullptr) { preorder(bt->right); }
+				return;
 			}
-			temp = temp->next;	//advances through the list
+			descendants(bt->left, newVal);
+			descendants(bt->right, newVal);
+		}
+	}
+	bool ancestordotcom(treeNode *bt, string key) { // part h
+		if (bt == nullptr) return false;
+		if (bt->name == key) return true;
+
+		if (ancestordotcom(bt->left, key) || ancestordotcom(bt->right, key)) {
+			cout << bt->name << " ";
+			return true;
+		}
+		return false;
+	}
+	//part i
+	treeNode* blackmirror(treeNode * bt) {
+		treeNode* tmp = new treeNode;
+		if (bt == nullptr) return nullptr;
+		tmp->name = bt->name;
+		
+		tmp->left = blackmirror(bt->right);
+		tmp->right = blackmirror(bt->left);
+
+		return tmp;
+	}
+	//part j
+	void lvlcmp(treeNode* og,  int lvl) {
+		if (og == nullptr) {
+			return;
+		}
+
+		if (lvl == 1) {
+			cout << og->name << " ";
+		}
+		else if (lvl > 1) {
+			lvlcmp(og->left, lvl-1);
+			lvlcmp(og->right, lvl-1);
+		}
+	}
+	void printlvls(treeNode* og, treeNode*cmp ) {
+		int tHeight = height(og);
+
+		for (int i = 1; i <= tHeight; i++) {
+			cout << "Lvl" << i << ": ";
+			lvlcmp(og,  i);
+			cout << "|| ";
+			lvlcmp(cmp, i);
+			std::cout << std::endl;
 		}
 	}
 };
 
 int main() {
-	int arry[7] = { 5,6,4,8,2,7,6 };	//int array for stack
-	int i = 1;
-	list que;	//so
-	for (int i = 0; i < 7; ++i) {	
-		que.push(arry[i]);	//initializes the list with the given array
+	bst tree, mirror_edge;
+	std::string months[12] = { "Jan", "Feb", "Mar" , "Apr" ,
+		"May" , "Jun" , "Jul" , "Aug" ,
+		"Sep" , "Oct" , "Nov" , "Dec" };
+	for (int i = 0; i < 12; i++) {	//populates bst
+		tree.insert(tree.bt_head, months[i]);
 	}
-	cout << "Start:  ";
-	que.print();	//initial order print
-
-	while (que.empty == false) {
-		cout << "Round " << i << ": ";
-		que.processedcheese(); //A function that mimics processing on the queue
-		que.print();
-		++i;
-	}
-	cout << "Round " << i << ": NULL" << endl;
+	cout << "Part b:\n";	//part b
+	tree.inorder(tree.bt_head);
+	cout << "\n\nPart c:\n";//part c
+	tree.dipside(tree.bt_head, 0);
+	cout << "\nPart d:\n";//part d
+	cout << "Height= " << tree.height(tree.bt_head);
+	cout << "\n\nPart e:\n";//part e
+	cout << "Left subtree height= " << tree.height(tree.bt_head->left);
+	cout << "\nRight subtree height= " << tree.height(tree.bt_head->right);
+	cout << "\n\nPart f:\n";//part f
+	cout << "Left subtree nodes= " << tree.nodes(tree.bt_head->left);
+	cout << "\nRight subtree nodes= " << tree.nodes(tree.bt_head->right);
+	cout << "\n\nPart g:\n";//part g
+	tree.descendants(tree.bt_head, "Mar");
+	cout << "\n\nPart h:\n";//part h
+	tree.ancestordotcom(tree.bt_head, "Sep");
+	cout << "\n\nPart i:\n";
+	mirror_edge.bt_head = tree.blackmirror(tree.bt_head);
+	mirror_edge.dipside(mirror_edge.bt_head, 0);
+	cout << "\n\nPart j:\n";
+	cout << "Orignial is printed then mirror:\n";
+	tree.printlvls(tree.bt_head, mirror_edge.bt_head);
 	system("pause");
 }
