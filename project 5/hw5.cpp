06f4cd6cb4565e689d54e34433beb0d28ad4115b
Fernando <fzavala649@gmail.com>
2018-02-28 21:55:21 -0800

    Update hw5.cpp

--- project 5/hw5.cpp
+++ project 5/hw5.cpp
@@ -1,4 +1,10 @@
-/* INFO */
+/*
+Authors:
+Fernando Cuevas
+Michael Rodriguez
+Class:   CPSC 335 - 12884, Algorithms, Tue/Thur
+Project: Assignment 5, Binary Search Trees
+*/
 
 #include <iostream>
 #include <iomanip>
