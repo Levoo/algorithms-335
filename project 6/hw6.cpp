defa16f88f2966df95f5156b26b4cffa2755de44
Fernando <fzavala649@gmail.com>
2018-03-05 19:34:56 -0800

    isBST still not workin, bstInsert not inserting

--- project 6/hw6.cpp
+++ project 6/hw6.cpp
@@ -12,14 +12,15 @@ struct node {
 };
 struct node2 {
 	string data;
-	node * left, *right;
+	node2 * left, *right;
 };
 
 class binaryTree {
 public:
-	node * root = nullptr;
-	node * root2 = nullptr;
-	
+	node * root = nullptr; // for binary tree question 1
+	node2 * t1 = nullptr; // the rest are for question 2
+	node2 * t2 = nullptr;
+
 	void createBT() { // man impleementation of bt found on paper
 		btInsert(root, 1, 4);
 		btInsert(root, 9, 5);
@@ -32,28 +33,51 @@ public:
 	void btInsert(node* bt, int newVal, int weight) {
 		if (root == nullptr) {//initializes head of bst
 			root = new node;
-			root = newNode(newVal, weight);
+			root = newBTNode(newVal, weight);
 			return;
 		}
 		else {
 			if (weight <= bt->weight) {
 				if (bt->left == nullptr) { //checks if there is no val in next node
 										   //populates and links if none
-					bt->left = newNode(newVal, weight);
+					bt->left = newBTNode(newVal, weight);
 				}
 				else { btInsert(bt->left, newVal, weight); }//traverse if a node exists
 			}
 			else {
 				if (bt->right == nullptr) { //checks if there is no val in next node
 											//populates and links if none
-					bt->right = newNode(newVal, weight);
+					bt->right = newBTNode(newVal, weight);
 				}
 				else { btInsert(bt->right, newVal, weight); }//traverse if a node exists
 			}
 		}
 	}
+
+	void bstInsert(node2* bt, std::string newVal) {
+		if (bt == nullptr) {//initializes head of bst
+			bt = newBSTNode(newVal);
+			return;
+		}
+		else {
+			if (newVal[0] <= bt->data[0]) {
+				if (bt->left == nullptr) { //checks if there is no val in next node
+										   //populates and links if none
+					bt->left = newBSTNode(newVal);
+				}
+				else { bstInsert(bt->left, newVal); }//traverse if a node exists
+			}
+			else {
+				if (bt->right == nullptr) { //checks if there is no val in next node
+											//populates and links if none
+					bt->right = newBSTNode(newVal);
+				}
+				else { bstInsert(bt->right, newVal); }//traverse if a node exists
+			}
+		}
+	}
 	//helper function for insert, mass is a flag variable so that we can reuse the newnode function for q2
-	node* newNode(int newVal, int mass) {
+	node* newBTNode(int newVal, int mass) {
 		node*  tmp = new node;
 		tmp->data = newVal;
 
@@ -62,22 +86,28 @@ public:
 		tmp->right = nullptr;
 		return tmp;
 	}
+	
+	node2* newBSTNode(std::string newVal) {
+		node2*  tmp = new node2;
+		tmp->data = newVal;
+		tmp->left = nullptr;
+		tmp->right = nullptr;
+		return tmp;
+	}
 
 	bool isBST(node* bt) {
 		if (bt == nullptr) return true;
-		if(bt->left != nullptr){ // 3 cases if bothL and R have value if only L has value if R has value only if only root has value
-			if(bt->right != nullptr){
+		if (bt->left != nullptr) { // 3 cases if bothL and R have value if only L has value if R has value only if only root has value
+			if (bt->right != nullptr) {
 				if (bt->left->data <= bt->data <= bt->right->data) {
-					 isBST(bt->left);
-					 isBST(bt->right);
+					isBST(bt->left);
+					isBST(bt->right);
 				}
 			}
 		}
-		
-
 		return false;
 	}
-	
+
 	int maxNode(node* bt, int max) {
 		if (bt == nullptr) return max; //return once you have the largest one
 
@@ -87,7 +117,7 @@ public:
 
 	}
 
-	void dipside(node *bt, int sp) {//part c
+	void dipside(node *bt, int sp) {
 		if (bt != nullptr) {
 			dipside(bt->right, sp += 5);
 			cout << setw(sp) << bt->data << endl;
@@ -95,31 +125,53 @@ public:
 		}
 	}
 
-	template<typename t2> // be able to call this with both int and string 
-	void preorder(t2* bt) {
+	template<typename t> // be able to call this with both int and string 
+	void preorder(t* bt) {
 		if (bt != nullptr) {
 			std::cout << bt->data << " ";//dont display bst as list
 			preorder(bt->left);
 			preorder(bt->right);
 		}
 	}
+
+	void inorder(node2* bt) {
+		if (bt == nullptr) return;
+
+		inorder(bt->left);
+		inorder(bt->right);
+		cout << bt->data << " ";
+	}
 };
 
 int main() {
 	binaryTree bt, bst;
-	//bt.createBT();
-	//bst.createBT();
-	bt.btInsert(bt.root, 5, 5);
-	bt.btInsert(bt.root, 4, 4);
-	bt.btInsert(bt.root, 3, 3);
-	bt.btInsert(bt.root, 6, 6);
-	bt.btInsert(bt.root, 7, 7);
-	bt.preorder(bt.root);
+	// question 1
+	cout << "Problem 1 a, b, c:\n";
+	bt.createBT(); //part 1.a 
+	cout << "Pre-order: ";
+	bt.preorder(bt.root); //part 1.b
+	bool isIt = bt.isBST(bt.root); // part 1.c
+	cout << "\nIs BST: " << isIt << endl;
+	cout << "\nMax: ";
+	cout << bt.maxNode(bt.root, 0) << endl; // part 1.d
 	cout << "\n";
-	cout << bt.maxNode(bt.root, 0) << endl;
-	cout << "\n";
-	bst.dipside(bst.root, 0);
-	bool isIt = bst.isBST(bst.root);
-	cout << isIt << endl;
+	// question 2
+	cout << "Problem 2 a, b, c:\n";
+	std::string months[12] = { "Jan", "Feb", "Mar" , "Apr" ,
+		"May" , "Jun" , "Jul" , "Aug" ,
+		"Sep" , "Oct" , "Nov" , "Dec" };
+	int j = 11;
+	for (int i = 0; i < 12; i++) {	//populates bst part 2.a currently not working
+		bst.bstInsert(bst.t1, months[i]);
+		bst.bstInsert(bst.t2, months[j--]);
+	}
+	cout << "t1 in-order: ";  // part b
+	bst.inorder(bst.t1); 
+	cout << "\nt2 in-order: ";
+	bst.inorder(bst.t2);
+	cout << "part c"; // probably need to construct the tree or something
+	// question 3
+	cout << "Problem 3:\n"; // no idea on how to approch this, will ask prof tomorrow
+	/...
 	system("pause"); // micheal scott
 }
